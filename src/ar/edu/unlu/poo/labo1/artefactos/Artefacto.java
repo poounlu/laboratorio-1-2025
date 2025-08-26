181d3b945fcd1553888603dc1307770f131512bd
LiuKaa <87624807+LiuKaa@users.noreply.github.com>
2025-08-26 20:53:40 -0300

    .

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,38 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private int poder;
+    private String tipo;
+
+    public Artefacto(String nombre,int poder,String tipo){
+        this.nombre = nombre;
+        this.poder = poder;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public boolean equals(String nombre2){
+        if(this.nombre.equals(nombre2)) return true;
+        else{
+            return false;
+        }
+    }
+
+    public int hashCode(){
+        return Objects.hash(this.nombre);
+    }
 }
