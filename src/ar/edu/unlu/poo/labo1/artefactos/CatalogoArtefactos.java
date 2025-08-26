59b3d6c9ca2df69815da073a37f2767b44a137ce
fede <federicocoyra@gmail.com>
2025-08-26 20:47:05 -0300

    Subo avance del trabajo en clase.

--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,26 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+
+    Set<Artefacto> catalogo = new HashSet<>();
+    public void agregarArtefacto (Artefacto artefacto){
+        catalogo.add(artefacto);
+    }
+
+    public Set obtenerArtefactosUnicos(){
+        return catalogo;
+    }
+
+    public List buscarArtefactosPorTipo(String tipo){
+        List<Artefacto> resultado = new ArrayList<>();
+        for (Artefacto a: artefacto){
+
+        }
+    }
+
+    public Map<String, Integer> contarArtefactosPorTipo(){
+
+    }
 }
