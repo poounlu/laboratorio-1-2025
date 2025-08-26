6a4a143795f942d3b9985feb8bc8ffc0eeea6e9a
alumno <alumno@unlu.edu.ar>
2025-08-26 20:57:44 -0300

    se entregan los avances del tp

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,49 @@
+
 package ar.edu.unlu.poo.labo1.artefactos;
+import ar.edu.unlu.poo.labo1.libreria.Libro;
+
+import java.awt.geom.Area;
+import java.util.Objects;
 
 public class Artefacto {
+    private String nombre;
+    private Integer poder;
+    private String tipo;
+
+
+    public Artefacto(String nombre, Integer poder, String tipo){
+        this.nombre = nombre;
+        if(poder >= 0 && poder <= 100){
+            this.poder = poder;
+        }
+        this.tipo = tipo;
+    }
+
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public Integer getPoder(){
+        return this.poder;
+    }
+    public String getTipo(){
+        return this.tipo;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artefacto artefacto = (Artefacto) o;
+        return Objects.equals(nombre, artefacto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        // 4. Generamos un hash code a partir de los mismos campos
+        return Objects.hash(nombre);
+    }
+
 }
+
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,39 @@
 package ar.edu.unlu.poo.labo1.artefactos;
+import ar.edu.unlu.poo.labo1.libreria.Libro;
+
+import java.util.*;
+
 
 public class CatalogoArtefactos {
+
+    private Artefacto artefacto;
+
+    Set<Artefacto> catalogoUnicos = new HashSet<>();
+    List<Artefacto> artefactosPorTipo = new ArrayList<>();
+    Map<String, Integer> cantArtefactosPorTipo = new HashMap<>();
+
+    public void agregarArtefacto(Artefacto artefacto) {
+
+             if (catalogoUnicos.add(artefacto)) {
+                 artefactosPorTipo.add(artefacto);
+             }
+            // cantArtefactosPorTipo.put(artefacto.getTipo() , 1);
+
+    }
+    public Set obtenerArtefactosUnicos(){
+        return catalogoUnicos;
+    }
+
+    /*public List buscarArtefactosPorTipo(String tipo){
+
+        return artefactosPorTipo;
+    }
+    public Map<String, Integer> contarArtefactosPorTipo(){
+        return cantArtefactosPorTipo;
+    }
+
+
+    public Artefacto obtenerArtefactoMasPoderoso(Artefacto artefacto){
+        return artefacto;
+    }*/
 }
