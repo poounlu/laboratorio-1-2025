6df49b4391b02b12beab06870b10ab755e446fe4
Estudiante <estudiante@gmail.com>
2025-08-26 20:54:49 -0300

    resultado final

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,42 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private int poder;
+    private String tipo;
+
+    public Artefacto(){}
+    public Artefacto(String nombre, int poder, String tipo) {
+        this.nombre = nombre;
+        this.poder = poder;
+        this.tipo = tipo;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+
+        if(obj == null || getClass() != obj.getClass()) return false;
+
+        Artefacto artefacto = (Artefacto) obj;
+        return Objects.equals(this.nombre,artefacto.nombre);
+    }
+
+    public int hashCode(){
+        return Objects.hash(this.nombre);
+    }
 }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,54 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+    private Set<Artefacto> catalogo = new HashSet<>();
+
+    public void agregarArtefacto(Artefacto artefacto){
+        this.catalogo.add(artefacto);
+    }
+
+    public Set<Artefacto> obtenerArtefactosUnicos(){
+        return this.catalogo;
+    }
+
+    public List<Artefacto> buscarArtefactosPorTipo(String tipo){
+        List<Artefacto> lista = new ArrayList<>(List.of());
+        for(Artefacto a : this.catalogo){
+            if(a.getTipo().equals(tipo)){
+                lista.add(a);
+            }
+        }
+        return lista;
+    }
+
+    public Map<String, Integer> contarArtefactosPorTipo(){
+        Map<String, Integer> artefactosPorTipo = new HashMap<>();
+        int conteo = 0;
+        for(Artefacto a : this.catalogo){
+            if(artefactosPorTipo.containsKey(a.getTipo())){
+                conteo = artefactosPorTipo.get(a.getTipo());
+                artefactosPorTipo.put(a.getTipo(), conteo + 1);
+            }
+
+        }
+
+        return artefactosPorTipo;
+    }
+
+    public Artefacto obtenerArtefactoMasPoderoso(){
+        int poder = 0;
+        Artefacto temp = new Artefacto();
+        if(this.catalogo.isEmpty()){
+            return null;
+        }
+        for(Artefacto a : this.catalogo){
+            if(a.getPoder() >= poder){
+                poder = a.getPoder();
+                temp = a;
+            }
+        }
+        return temp;
+    }
 }
