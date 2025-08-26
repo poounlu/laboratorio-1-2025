400a37d68d6940d6a618b18bfedd0302854a102d
valentin <quelle2626@gmail.com>
2025-08-26 20:41:47 -0300

    Grupo Valentin Quelle, Agustin Daddario Provisorio

--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,39 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Collections;
+
 public class CatalogoArtefactos {
+
+    HashSet<Artefacto> catalogo = new HashSet<>();
+    List<Artefacto> misArtefactos = new ArrayList<>();
+
+    public void agregarArtefacto(Artefacto artefacto){
+        catalogo.add(artefacto);
+    }
+
+    public Set obtenerArtefactosUnicos() {
+
+        return catalogo;
+    }
+
+    public List buscarArtefactosPorTipo(String tipo){
+
+          for (Artefacto artefacto : catalogo){
+              if ((artefacto.getTipo()).equals(tipo)){
+                  misArtefactos.add(artefacto);
+              }
+              for (artefacto : misArtefactos){
+                  misArtefactos.sort(artefacto.getPoder());
+
+              }
+          }
+
+    }
+
+
+
 }
