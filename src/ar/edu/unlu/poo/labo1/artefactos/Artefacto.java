0da4983427f81636ad6faf164bc4680d5caa0415
alumno <inventado@gmail.com>
2025-08-26 20:55:02 -0300

    nuestro avance

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,42 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.Objects;
+
 public class Artefacto {
-}
+    private String nombre;
+    private int nivelPoder;
+    private String tipo;
+
+    public Artefacto(String unNombre, int unNivel, String unTipo){
+        this.nombre = unNombre;
+        this.nivelPoder = unNivel;
+        this.tipo = unTipo;
+    }
+
+    //Metodo-----------------------------------------------------------------
+    public boolean equals(Object o){
+        if(this == o){
+            System.out.println("Es igual");
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()) return false;
+        Artefacto artefacto = (Artefacto) o;
+        return Objects.equals(nombre, artefacto.nombre);
+    }
+
+    public int hashCode(){
+        return Objects.hash(nombre);
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getPoder(){
+        return nivelPoder;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+ }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,37 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+
+    Set<Artefacto> catalogo = new HashSet<>();
+
+    public CatalogoArtefactos(){
+
+    }
+
+    //metodo--------------------------------------------------------
+    public void agregarArtefacto(Artefacto unArtefacto){
+        unArtefacto.equals();
+        catalogo.add(unArtefacto);
+    }
+
+
+    public Set obtenerArtefactosUnicos(){
+        return catalogo;
+    }
+
+    public List buscarArtefactosPorTipo(String unTipo){
+
+    }
+
+    public Map contarArtefactosPorTipo(){
+
+    }
+
+    public Artefacto obtenerArtefactoMasPoderoso(String unTipo){
+        return unTipo;
+    }
+
+
 }
