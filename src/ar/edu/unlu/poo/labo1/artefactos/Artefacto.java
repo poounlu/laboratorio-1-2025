b7cad5228e9271ef148563b26a49977c2187b0c2
Juanchumu <ricard9045@gmail.com>
2025-08-26 19:52:41 -0300

    1

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,41 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.Objects;
+
 public class Artefacto {
+	String nombre;
+	int poder;
+	String tipo; 
+
+	public Artefacto(String nombre, int poder, String tipo){
+		this.nombre = nombre;
+		this.poder = poder;
+		this.tipo = tipo;
+	}
+	@Override
+	public boolean equals(Object o){
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass() ) return false;
+		Artefacto test = (Artefacto) o;
+		boolean estado;
+		estado =  Objects.equals(o, test.nombre);
+			//((Objects.equals(nombre, test.nombre))&&((
+							//Objects.equals(poder, test.poder)
+							//)&&(
+							//	Objects.equals(tipo, test.tipo
+							//	))));
+		return estado;
+
+	}
+	@Override
+	public int hashCode(){
+		return Objects.hash(nombre);
+	}
+
+
+
+
+
+
+
 }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,16 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.HashSet;
+
 public class CatalogoArtefactos {
+	Set<Artefacto> artefactosUnicos = new HashSet<>();
+
+	public void	agregarArtefacto(Artefacto item){
+
+		this.artefactosUnicos.add(item);
+
+	}
+	public Set<Artefacto> obtenerArtefactosUnicos(){
+
+	}
 }
