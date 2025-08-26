4a3fdebf8bd12826b9c1c9d45c6bf6876f829323
tomi <reytomas1nm@gmail.com>
2025-08-26 20:58:08 -0300

    pruebas2

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,53 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private Integer poder;
+    private String tipo;
+
+public Artefacto(String nombre,int poder,String tipo) {
+    this.nombre = nombre;
+    this.poder = poder;
+    this.tipo = tipo;
+}
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPoder() {
+        return poder;
+    }
+
+    public void setPoder(Integer poder) {
+        this.poder = poder;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artefacto art1 = (Artefacto) o;
+        return Objects.equals(nombre, art1.nombre);
+    }
+    @Override
+    public int hashCode(){
+    return Objects.hash(nombre);
+    }
+
 }
